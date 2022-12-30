6090282fc859f1ec6eeb4547ece2f81eb01c2d74
amir hussein khajeh <axajeh@gmail.com>
2022-12-31 02:30:16 +0330

    [CREATE]: basic shape action

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
-options = [
-        {
-            "message": "Calculate area and perimeter of the circle",
-        },
-        {
-            "message": "Calculate area and perimeter of the square",
-        },
-        {
-            "message": "Calculate area and perimeter of the triangle",
-        },
-        {
-            "message": "Calculate area and perimeter of the trapezoid",
-        },
-        {
-            "message": "Calculate area and perimeter of the polygon",
-        },
-        {
-            "message": "Calculate area and perimeter of the rectangle",
-        },
-        {
-            "message": "Calculate volume of the sphere",
-        },
-        {
-            "message": "Calculate volume of the cone",
-        },
-        {
-            "message": "Calculate area and perimeter of the oval",
-        },
+def calc():
+    print("hello")
+
+
+class Action:
+    def __init__(self, message, run):
+        self.message = message
+        self.run = run
+
+
+actions = [
+        Action("Calculate area and perimeter of the circle", calc),
+        Action("Calculate area and perimeter of the square", calc),
+        Action("Calculate area and perimeter of the triangle", calc),
+        Action("Calculate area and perimeter of the trapezoid", calc),
+        Action("Calculate area and perimeter of the polygon", calc),
+        Action("Calculate area and perimeter of the rectangle", calc),
+        Action("Calculate volume of the sphere", calc),
+        Action("Calculate volume of the cone", calc),
+        Action("Calculate area and perimeter of the oval", calc)
     ]
 
-
 def get_action():
-    for index in range(len(options)):
-        option = options[index]
-        print("options #{}: {}".format(index + 1, option.get("message")))
+    for index in range(len(actions)):
+        action = actions[index]
+        print("options #{}: {}".format(index + 1, action.message))
+
+    selected_actions = input("Enter number of desired action: ")
+
+    try:
+        selected_actions_index = int(selected_actions) - 1
+        return actions[selected_actions_index]
+
+    except NameError:
+        print("Invalid action selected, pleas try again")
+        get_action()
 
 
 if __name__ == '__main__':
-    get_action()
+    print(get_action().message)
