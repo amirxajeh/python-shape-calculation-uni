e91029fa3682ca9fe1bd573b184e2903897e0a98
amir hussein khajeh <axajeh@gmail.com>
2022-12-31 01:55:57 +0330

    INITIAL commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def show_(name):
+    match name:
+        case "amir":
+            return "amir x"
+        case _:
+            return "nothing"
+
+
+if __name__ == '__main__':
+    print(print_hi('PyCharm'))
