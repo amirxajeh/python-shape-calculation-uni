cfa415e41ea5f1779224887575af71d634500cc0
amir hussein khajeh <axajeh@gmail.com>
2022-12-31 02:14:11 +0330

    [CREATE]: show options

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
-def show_(name):
-    match name:
-        case "amir":
-            return "amir x"
-        case _:
-            return "nothing"
+options = [
+        {
+            "message": "Calculate area and perimeter of the circle",
+        },
+        {
+            "message": "Calculate area and perimeter of the square",
+        },
+        {
+            "message": "Calculate area and perimeter of the triangle",
+        },
+        {
+            "message": "Calculate area and perimeter of the trapezoid",
+        },
+        {
+            "message": "Calculate area and perimeter of the polygon",
+        },
+        {
+            "message": "Calculate area and perimeter of the rectangle",
+        },
+        {
+            "message": "Calculate volume of the sphere",
+        },
+        {
+            "message": "Calculate volume of the cone",
+        },
+        {
+            "message": "Calculate area and perimeter of the oval",
+        },
+    ]
+
+
+def get_action():
+    for index in range(len(options)):
+        option = options[index]
+        print("options #{}: {}".format(index + 1, option.get("message")))
 
 
 if __name__ == '__main__':
-    print(print_hi('PyCharm'))
+    get_action()
